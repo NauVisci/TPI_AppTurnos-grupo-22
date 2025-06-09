a34f0e5def511ebd136ebcedc049caad5c2227ac
NahuelV <nahuel.martin.viscito@gmail.com>
2025-06-09 19:40:57 -0300

    Subo file con el codigo para crear la base de datos, tiene algunas correcciones que vimos con Alex

---
+++ CreateDB_GestionTurnos.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE GestionTurnos
+GO
+
+USE GestionTurnos
+GO
+
+CREATE TABLE Servicios (
+    IdServicio INT PRIMARY KEY,
+    Nombre NVARCHAR(255),
+    Duracion DECIMAL(10, 2),
+    Precio DECIMAL(10, 2)
+	);
+
+CREATE TABLE GestionDeRoles (
+    IdRol INT PRIMARY KEY,
+    NombreRol NVARCHAR(255)
+	);
+
+CREATE TABLE Clientes (
+    IdCliente INT PRIMARY KEY,
+	Nombre NVARCHAR(50),
+	Apellido NVARCHAR(50),
+	Email NVARCHAR(100),
+    Domicilio NVARCHAR(255),
+    Telefono NVARCHAR(50),
+    FechaCreacion DATE,
+    DNI NVARCHAR(50),
+	IdRol INT,
+
+	FOREIGN KEY (IdRol) REFERENCES GestionDeRoles(IdRol)
+	);
+
+
+CREATE TABLE Empleados (
+    IdEmpleado INT PRIMARY KEY,
+    Nombre NVARCHAR(50),
+	Apellido NVARCHAR(50),
+    Especialidad NVARCHAR(100),
+    FechaAltaEmpleado DATE,
+	IdRol INT,
+
+	FOREIGN KEY (IdRol) REFERENCES GestionDeRoles(IdRol)
+	);
+
+CREATE TABLE Turnos (
+    IdTurno INT PRIMARY KEY,
+	IdCliente INT,
+	IdServicio INT,
+	IdEmpleado INT,
+    ProfesionalElegido NVARCHAR(255),
+    Estado NVARCHAR(50),
+    Fecha DATETIME,
+    Valoracion DECIMAL(4, 2)
+
+    FOREIGN KEY (IdCliente)  REFERENCES Clientes(IdCliente),
+    FOREIGN KEY (IdServicio) REFERENCES Servicios(IdServicio),
+	FOREIGN KEY (IdEmpleado) REFERENCES Empleados(IdEmpleado)
+	);
+
+CREATE TABLE Estadisticas (
+    IdEstadistica INT PRIMARY KEY,
+	IdEmpleado INT,
+    TurnosAtendidos INT,
+    IngresosGenerados DECIMAL(10,2),
+	PromedioCalificacion DECIMAL(4, 2),
+	PromedioTurnosMes INT, -- TIENE QUE SER INT EL TIPO DE DATO, ANTERIOR DECIMAL
+
+	FOREIGN KEY (IdEmpleado) REFERENCES Empleados(IdEmpleado),
+	);
+
+	DROP TABLE Estadisticas;
+	DROP DATABASE AppPeluqueria;
